9738244ea2857c30948366b61420f992a19eda62
DAVIDMWELU <davidmwelu@gmail.com>
2025-01-17 09:57:34 +0300

    Add fetchBooks implementation and improve renderBooks

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  // Fetch books from the API and return the Promise
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok ' + response.statusText);
+      }
+      return response.json();
+    })
+    .then(data => {
+      renderBooks(data);
+      return data; // Ensure the test can verify the returned data
+    })
+    .catch(error => console.error('There was a problem with the fetch operation:', error));
 }
 
 function renderBooks(books) {
